c9f7027ab0784a786ee5f5547216017b3cb7cf37
Mushegh Petrosyan <p.mushegh@gmail.com>
2017-10-20 13:38:12 +0400

    Some not working changes.

--- src/test/java/GetTest.java
+++ src/test/java/GetTest.java
@@ -1,20 +1,42 @@
 //package CountriesRestTests;
 
 import org.testng.Assert;
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
 import static com.jayway.restassured.RestAssured.get;
 
 import com.jayway.restassured.response.Response;
 import org.json.JSONArray;
 import org.json.JSONException;
 
+import org.mockserver.integration.ClientAndProxy;
+import org.mockserver.integration.ClientAndServer;
+
+import static org.mockserver.integration.ClientAndProxy.startClientAndProxy;
+import static org.mockserver.integration.ClientAndServer.startClientAndServer;
+
 public class GetTest {
+
+    private ClientAndProxy proxy;
+    private ClientAndServer mockServer;
+
+
+    @BeforeClass
+    public void startProxy() {
+        mockServer = startClientAndServer(1080);
+        proxy = startClientAndProxy(1090);
+    }
+
+    @AfterClass
+    public void stopProxy() {
+        proxy.stop();
+        mockServer.stop();
+    }
 
     @Test
     public void getRequestFindCapital() throws JSONException {
 
         // выполняем запрос get для доступа ко всем параметрам ответа
-        Response resp = get("http://restcountries.eu/rest/v1/name/ukrain");
+        Response resp = get("localhost:1080/");
 
         Assert.assertEquals(resp.getStatusCode(), 200, "Response status code is incorrect.");
 
@@ -25,12 +47,6 @@ public class GetTest {
 
         // проверка, что столицей является Осло
         Assert.assertEquals(capital, "Kiev", "Wrong country capital is found.");
-    }
-
-
-    @Test
-    public void testingSomething() {
-
     }
 
 }
